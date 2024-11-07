4a0efed4de11350d53256a17d700c48814deb2e6
nicoleli04 <nicoleliyh@gmail.com>
2024-11-07 14:08:31 -0800

    Create project Skeleton for Backend API Endpoints

---
+++ backend/src/api/routes/profile.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getProfile, createProfile, editProfile } from "../../controller/profile";
+const router: Router = Router();
+export default (app: Router) => {
+    router.get("/profile", getProfile);              // Get user profile
+    router.post("/profile", createProfile);          // Create a new profile
+    router.put("/profile/edit", editProfile);        // Edit profile details
+    app.use("/", router);
+};
