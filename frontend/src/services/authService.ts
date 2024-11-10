633064ac9b1076e13a90fdf8fda550ddd1f1e93f
haleyfchen1 <haley@dcube.ai>
2024-11-10 13:16:14 -0800

    Incomplete attempt at user info saving.

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,26 @@
+// src/services/authService.ts
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3000'; 
+
+
+//SIGN-UP
+export const register = async (email: string, password: string) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/register`, { email, password });
+    console.log(response.data);
+    return response.data;
+  } catch (error: any) {
+    throw error.response?.data || error.message;
+  }
+};
+
+//LOGIN
+export const login = async (email: string, password: string) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/login`, { email, password });
+    return response.data;
+  } catch (error: any) {
+    throw error.response?.data || error.message;
+  }
+};
