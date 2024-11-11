f067948267c6509bd3a40cdee8461a1095d8a3bd
FongChanatip <fongchanatip@gmail.com>
2024-11-10 18:46:52 -0800

    Added middleware to protected endpoint

--- backend/src/api/routes/profile.ts
+++ backend/src/api/routes/profile.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getProfile, createProfile, editProfile } from "../../controller/profile";
+import { requireAuthorization } from "../../middlewares/user";
 
 
 const router: Router = Router();
 export default (app: Router) => {
-    router.get("/profile", getProfile);              // Get user profile
-    router.post("/profile", createProfile);          // Create a new profile
-    router.put("/profile/edit", editProfile);        // Edit profile details
-    app.use("/", router);
+    router.get("/", requireAuthorization, getProfile);              // Get user profile
+    router.post("/", requireAuthorization, createProfile);          // Create a new profile
+    router.put("/edit", requireAuthorization, editProfile);        // Edit profile details
+    app.use("/profile", router);
 };
