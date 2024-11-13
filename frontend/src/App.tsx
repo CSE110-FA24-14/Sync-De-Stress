bfd2010649f034adc6041f708439e3b532ca4263
GunyoungPark <94301057+GunyoungPark@users.noreply.github.com>
2024-11-12 18:57:34 -0800

    now with create profile redirection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
+import CreateProfile from './pages/CreateProfile';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<Login />} />
+        <Route path="/CreateProfile" element={<CreateProfile />} />
       </Routes>
     </Router>
   );
