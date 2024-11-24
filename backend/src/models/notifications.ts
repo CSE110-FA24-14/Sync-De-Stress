06e5d485a17eff292bfba3b11d00069efa8133c6
FongChanatip <fongchanatip@gmail.com>
2024-11-24 15:05:53 -0800

    Update Notification model

--- backend/src/models/notifications.ts
+++ backend/src/models/notifications.ts
@@ -2,21 +2,21 @@ import { Schema, model } from "mongoose";
 import { NotificationInterface } from "../shared/interface/modelInterface";
 
 const NotificationSchema: Schema<NotificationInterface> = new Schema({
-    "message": {
+    "userId": {
         type: String,
         required: true
     },
     "type": {
-        type: String,
+        type: Number,
         required: true
     },
-    "icon": {
+    "targetId": {
         type: String,
         default: ""
     },
-    "actionLink": {
-        type: String,
-        default: ""
+    "date": {
+        type: Date,
+        default: Date.now
     }
 }, { versionKey: false });
 
